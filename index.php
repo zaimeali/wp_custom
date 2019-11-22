b8f0960219f3889b5590f6d5c82699861117526b
Zaime Ali <zaimeali1997@gmail.com>
2019-11-22 22:29:37 +0500

    First Commit

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    	<meta charset="utf-8">
+    	<meta name="description" content="The best in cats">
+    	<meta name="keywords" content="Cats, code, lolz">
+		<title>Cats Who Code | The Best in Cats</title>
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+
+
+	
+		<div id="header">
+			<h1><a href="#">Cats Who Code</a></h1>
+		</div><!-- #header --> 
+
+		<div class="navcontainer">
+			<div class="inner-navcontainer">
+				<nav class="clearfix">
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">Cat of the Week</a></li>
+							<li><a href="#">Location</a></li>
+							<li><a href="#">About Us</a></li>
+						</ul>
+				</nav>
+			</div>
+		</div>
+
+
+
+
+		<div id="container">
+			
+			<div id="main">
+	
+				<div class="post">
+					<h2><a href="#">Purred Programming</a></h2>
+					<img src="images/cats1.png" alt="NYC Startups">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a consectetur purus. Pellentesque at leo erat, id pellentesque sapien. Cras id purus sem. Nulla lacinia consequat nulla, vitae porttitor urna venenatis id. Nam iaculis risus ac mauris vestibulum malesuada. Nulla dolor mi, cursus sed ornare vel, feugiat sed nibh. Suspendisse vitae pulvinar ligula. In eu dui erat. Etiam sit amet malesuada turpis. Duis dapibus feugiat euismod. Suspendisse vitae consequat nunc. Vivamus aliquet vulputate varius.</p>
+					
+					<span class="post-meta">Posted by <a href="#">Chris Castig</a> on August 2nd</span>
+
+				</div><!-- .post --> 
+
+				<div class="post">
+					<h2><a href="#">Cat's Eye</a></h2>
+					<img src="images/cats2.png" alt="Courage">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin a consectetur purus. Pellentesque at leo erat, id pellentesque sapien. Cras id purus sem. Nulla lacinia consequat nulla, vitae porttitor urna venenatis id. Nam iaculis risus ac mauris vestibulum malesuada. Nulla dolor mi, cursus sed ornare vel, feugiat sed nibh. Suspendisse vitae pulvinar ligula. In eu dui erat. Etiam sit amet malesuada turpis. Duis dapibus feugiat euismod. Suspendisse vitae consequat nunc. Vivamus aliquet vulputate varius. Boomer lives!</p>	
+
+					<span class="post-meta">Posted by <a href="#">Chris Castig</a> on July 20th</span>
+				</div><!-- .post --> 
+
+			</div><!-- #main --> 
+
+
+
+
+
+			<div id="sidebar" class="clearfix">
+
+				<div class="widget">
+					<h3>Archive</h3>
+					<ul>
+						<li><a href="#">January</a></li>
+						<li><a href="#">February</a></li>
+						<li><a href="#">March</a></li>
+						<li><a href="#">April</a></li>
+						<li><a href="#">May</a></li>
+						<li><a href="#">June</a></li>
+						<li><a href="#">July</a></li>
+					</ul>
+				</div><!-- .widget --> 
+
+				<div class="widget">
+					<h3>Blogroll</h3>
+					<ul>
+						<li><a href="#">Mailchimp</a></li>
+						<li><a href="#">One Munch</a></li>
+						<li><a href="#">Hacker Mews</a></li>
+						<li><a href="#">Soylent Cat Food</a></li>
+						<li><a href="#">Blue Buffalo</a></li>
+					</ul>
+				</div><!-- .widget --> 
+
+				<div class="widget">
+					<a href="#">
+						<img src="images/ad-submit-your-own.png" alt="Submit Your Own">
+					</a>
+				</div><!-- .widget --> 
+
+				<div class="widget">
+					<a href="#">
+						<img src="images/ad-tshirts.png" alt="Cats Who Code Tees">
+					</a>
+				</div><!-- .widget --> 
+
+
+			</div><!-- #sidebar -->
+
+
+
+
+
+
+			
+			<div class="clear"></div>
+			<div id="footer">
+
+				<a href="https://twitter.com/onemonthedu">
+					<img src="images/social-icon-twitter.png" alt="Twitter">
+				</a>
+
+				<a href="#">
+					<img src="images/social-icon-facebook.png" alt="Facebook">
+				</a>
+				
+			</div><!-- #footer -->
+
+		</div><!-- #container --> 
+	</body>
+</html>
